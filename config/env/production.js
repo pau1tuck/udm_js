e45d25a1fbda5ca6c954dc5382ab26bba2b4130b
Paul Tuck <68965131+pau1tuck@users.noreply.github.com>
2020-08-29 00:24:40 +0800

    Update production.js

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://admin:Susandogwater7!@ds047782.mongolab.com:47782/udm',
+	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://[...]',
 	assets: {
 		lib: {
 			css: [
